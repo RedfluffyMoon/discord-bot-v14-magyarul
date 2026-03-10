0dcd2ede45ad398c41608729dbf7150680f0a687
RedfluffyMoon <darknesytb@gmail.com>
2026-03-10 20:15:32 +0100

    ping.js

---
+++ ping.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('🏓 Megmutatja a bot késleltetését'),
+
+  async execute(interaction) {
+    const sent = await interaction.reply({ content: '🏓 Pinging...', fetchReply: true });
+    await interaction.editReply(
+      `🏓 **Pong!**\n📡 Latency: \`${sent.createdTimestamp - interaction.createdTimestamp}ms\`\n💓 API: \`${interaction.client.ws.ping}ms\``
+    );
+  }
+};
