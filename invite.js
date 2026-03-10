fb904598a7e0d2a27124e3ab024bd6bfb30a25c6
RedfluffyMoon <darknesytb@gmail.com>
2026-03-10 20:29:29 +0100

    invite.js

---
+++ invite.js
@@ -0,0 +1,18 @@
+// commands/general/invite.js
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('invite')
+    .setDescription('🔗 Bot meghívó link'),
+
+  async execute(interaction) {
+    const link = `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot%20applications.commands`;
+    const embed = new EmbedBuilder()
+      .setTitle('🔗 Bot meghívása')
+      .setDescription(`[Kattints ide a bot meghívásához!](${link})`)
+      .setColor('#5865F2')
+      .setTimestamp();
+    await interaction.reply({ embeds: [embed] });
+  }
+};
