c7cb9c7659c86a32cef7f4a664bcc6244fc920eb
RedfluffyMoon <darknesytb@gmail.com>
2026-03-11 07:21:24 +0100

    Create hello command file

---
+++ commands/general/hello.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('hello')
+    .setDescription('👋 Üdvözöl a bot'),
+
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setTitle('👋 Helló!')
+      .setDescription(`Szia ${interaction.user}! Örülök, hogy itt vagy! 😊`)
+      .setColor('#5865F2')
+      .setTimestamp();
+    await interaction.reply({ embeds: [embed] });
+  }
+};
